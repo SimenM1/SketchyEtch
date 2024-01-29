9959597ac7fc97e01e3e2f679feee81b2f32a0d5
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-01-29 13:53:05 +0000

    color chainging on every hover added

--- javascript.js
+++ javascript.js
@@ -2,7 +2,6 @@ let gridSize = 5;
 let numberDivs = gridSize*gridSize;
 createGrid()
 colorChanger()
-
 wrapper = document.querySelector("#wrapper");
 
 function createGrid() {
@@ -17,10 +16,20 @@ function createGrid() {
 function promptFn() {
     wrapper.innerHTML = "";
     gridSize = prompt("specify grid size(x by x) *must be less than 20*")
+    if (gridSize > 20) {
+        return alert("size too large")
+    }
+    else
     numberDivs = gridSize*gridSize;
     createGrid()
     colorChanger()
     return gridSize
+}
+
+function rgbNr() {
+    nOne = Math.random()*256
+    n = Math.floor(nOne)
+    return n 
 }
 
 
@@ -29,13 +38,16 @@ function colorChanger() {
     divCol = document.querySelectorAll("div.divSub")
 
     divCol.forEach(divSub => {
-        divSub.addEventListener("mouseenter", () =>
-        divSub.style.backgroundColor = "black",
+        divSub.addEventListener("mouseenter", () => {
+        r = rgbNr();
+        g = rgbNr();
+        b = rgbNr();
+        divSub.style.backgroundColor = "rgb(" + r + " ," + g + " ," + b + ")";
         divSub.addEventListener("mouseout", () => {
-            setTimeout(() => divSub.style.backgroundColor = "white", 500)
+            setTimeout(() => divSub.style.backgroundColor = "white", 500);
             })
 
-        )
+        })
     });
 }
 
