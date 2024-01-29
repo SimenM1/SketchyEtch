210b166addfaca86784f9b15d19763d65e2dff02
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-01-29 12:24:34 +0000

    restructed to add button to change grid size

--- javascript.js
+++ javascript.js
@@ -1,26 +1,41 @@
-const gridSize = prompt("specify grid size(x by x)");
-const numberDivs = gridSize*gridSize;
+let gridSize = 5;
+let numberDivs = gridSize*gridSize;
+createGrid()
+colorChanger()
 
 wrapper = document.querySelector("#wrapper");
 
-for (i=0; i < numberDivs; i++) {
+function createGrid() {
+    for (i=0; i < numberDivs; i++) {
     divs = document.createElement("div");
     divs.classList.add("divSub");
     divs.style.cssText = "width: " + 960/(+gridSize) + "px; height: " + 960/(+gridSize) + "px;";
     wrapper.appendChild(divs);
 }
+}
+
+function promptFn() {
+    wrapper.innerHTML = "";
+    gridSize = prompt("specify grid size(x by x) *must be less than 20*")
+    numberDivs = gridSize*gridSize;
+    createGrid()
+    colorChanger()
+    return gridSize
+}
 
 
 
-divCol = document.querySelectorAll("div.divSub")
+function colorChanger() {
+    divCol = document.querySelectorAll("div.divSub")
 
-divCol.forEach(divSub => {
-    divSub.addEventListener("mouseenter", () =>
-    divSub.style.backgroundColor = "black",
-    divSub.addEventListener("mouseout", () => {
-        setTimeout(() => divSub.style.backgroundColor = "white", 500)
-        })
+    divCol.forEach(divSub => {
+        divSub.addEventListener("mouseenter", () =>
+        divSub.style.backgroundColor = "black",
+        divSub.addEventListener("mouseout", () => {
+            setTimeout(() => divSub.style.backgroundColor = "white", 500)
+            })
 
-    )
-});
+        )
+    });
+}
 
