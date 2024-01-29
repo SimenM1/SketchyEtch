976a88e05959d6a1c2e54905944d8e7447d9b1f1
SimenM1 <156329770+SimenM1@users.noreply.github.com>
2024-01-29 11:24:59 +0000

    working game, changed grid to fixed size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,26 @@
-wrapper = document.querySelector("#wrapper")
-const gridSize = 16
-const numberDivs = gridSize*gridSize
-wrapper.style.cssText = "width: " + gridSize*10 + "px; height: auto; flex-wrap: wrap; flex-direction: row;";
+const gridSize = prompt("specify grid size(x by x)");
+const numberDivs = gridSize*gridSize;
+
+wrapper = document.querySelector("#wrapper");
+
 for (i=0; i < numberDivs; i++) {
-divs = document.createElement("div")
-wrapper.appendChild(divs)
+    divs = document.createElement("div");
+    divs.classList.add("divSub");
+    divs.style.cssText = "width: " + 960/(+gridSize) + "px; height: " + 960/(+gridSize) + "px;";
+    wrapper.appendChild(divs);
 }
+
+
+
+divCol = document.querySelectorAll("div.divSub")
+
+divCol.forEach(divSub => {
+    divSub.addEventListener("mouseenter", () =>
+    divSub.style.backgroundColor = "black",
+    divSub.addEventListener("mouseout", () => {
+        setTimeout(() => divSub.style.backgroundColor = "white", 500)
+        })
+
+    )
+});
+
